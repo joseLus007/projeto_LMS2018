5771425d4be267ec8c5f6647441c2934f78096b7
Jose Luis Merma Pinedo <1800780@SALAS.AULAS>
2018-10-29 11:26:40 -0300

    cursos

---
+++ ProjectDiangou/core/Templates/scripts/01.js
@@ -0,0 +1,12 @@
+var lista=document.querySelector('sem');
+lista.onclick=function(){
+    var local = lista.getAttribute('s1');
+    if (local==='s1'){
+        lista.setAttribute('s2','di2'){
+    }else{
+        lista.setAttribute('s1','div1')
+        }
+    }
+    document.getElementById('s1').onclick=function(){
+        var conter=document.getElementById('')
+    }
