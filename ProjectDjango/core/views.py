41ea383892b0a1fc80769b56a51545dff35f4f97
joseLus007 <42036755+joseLus007@users.noreply.github.com>
2018-10-28 22:55:30 -0300

    estrutura

---
+++ ProjectDjango/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'E-Commerce FIT',
+        'paragrafo1': 'Primeiro parágrafo aqui',
+        'paragrafo2': 'Segundo parágrafo aqui',
+        'alunos': ['Ricardo','Marco','Marco Chocolate','Kleverson','Predo'],
+        'alunos2': [],
+        'condicao1': True,
+        'condicao2': False,
+        'lista': [1,2,3,4,5]
+    }
+    return render(request,"index.html",context)
